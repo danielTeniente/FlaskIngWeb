74db6d6a0be3b279f40610572bbf403dd83e4853
Daniel <daniel.diaz.bedoya@udla.edu.ec>
2019-09-30 16:32:10 -0500

    CRUD terminado

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+	"""docstring for Task"""
+	id=db.Column(db.Integer, primary_key=True)
+	content=db.Column(db.String(200))
+	done=db.Column(db.Boolean)
+		
+
 
 @app.route("/")
 def index():
-    return "Hello World!"
-@app.route("/hola/<int:num>")
-def hola(num):
-    return "Hola {}".format(num)
+	tasks=Task.query.all()
+	return render_template('index.html', list=tasks)
+
+
+@app.route("/create-task",methods=['POST'])
+def create():
+    task=Task(content=request.form['content'], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route("/save-task/<id>",methods=['POST'])
+def save(id):
+	task=Task.query.filter_by(id=int(id)).first()
+	task.content=request.form['content']
+	db.session.commit()
+	return redirect(url_for('index'))
+
+@app.route('/done/<id>')
+def done(id):
+	task=Task.query.filter_by(id=int(id)).first()
+	task.done=not(task.done)
+	db.session.commit()
+	return redirect(url_for('index'))
+
+
+@app.route("/edit/<id>")
+def edit(id):
+	task=Task.query.filter_by(id=int(id)).first()
+	return render_template('edit.html', task=task)
+
+@app.route("/delete/<id>")
+def delete(id):
+	#task=Task.query.filter_by(id=int(id)).first()
+	task=Task.query.filter_by(id=int(id)).delete()
+	db.session.commit()
+	return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.run(debug=True)
